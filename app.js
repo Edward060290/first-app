538560db06eda404642f20b2b433d1b75739c989
Edward060290 <barrnoneduardo060290@gmail.com>
2018-02-07 00:20:27 -0600

    crate the app use

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var bodyParser = require("body-parser");
 var path = require("path")
 
 var app = express();
+
+app.use(bodyParser.jason());
+app.use(bodyParser.urlendcoded({extend:false}));
+
+app.get('/', function(req, res){
+  res.send("hello World")
+});
+
 app.listen(3000,function(req, resp){
   console.log("server started at 3000");
 })
