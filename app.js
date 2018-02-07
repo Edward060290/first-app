abac53feccbd0a171caeb4a978ee4e21ae308cec
Edward060290 <barrnoneduardo060290@gmail.com>
2018-02-06 23:44:19 -0600

    finish setting up express server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var http = require("http");
+var bodyParser = require("body-parser");
+var path = require("path")
 
-var http = espress();
-
+var app = express();
 app.listen(3000,function(req, resp){
-  
+  console.log("server started at 3000");
 })
